9278f05c235666582901726a2b23dcf9494906db
Daniel <danielpinck@gmail.com>
2024-04-11 13:16:13 +0200

    trying new code

--- wordle.php
+++ wordle.php
@@ -4,40 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+  <style>
+    .correct {
+      background-color: rgb(56, 211, 85);    
+    }
+
+    .standard {
+    background-color: white;
+    }
+
+    .wrong {
+    background-color: rgb(219, 54, 54);
+    }
+  </style>
 </head>
 <body>
-<form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post">
+  <form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post">
 
 
-<?php 
+  <?php 
 
     include "wordle_functions.php";
     $array_variable = splitWords($wordList);
     arrayOutput($array_variable);
-?>
-<br>
-<br>
-<?php 
+  ?>
+  <br>
+  <br>
+  <?php 
     $randomWord = randomWord($array_variable);
     foreach ($randomWord as $letter) {
-        echo $letter;
+      echo $letter;
     }
     $randomWordTest = "TISCH";
     echo "<br>";
-    
+      
     echo $randomWordTest;
-    
-?>
-<br><br>
-<input type="Text" name="letter_1" maxlength="1" size="1">
-<input type="Text" name="letter_2" maxlength="1" size="1">
-<input type="Text" name="letter_3" maxlength="1" size="1">
-<input type="Text" name="letter_4" maxlength="1" size="1">
-<input type="Text" name="letter_5" maxlength="1" size="1">
-<input type="Submit" value="Guess"/>
-<?php 
-guessWord($randomWordTest);
-?>
+      
+  ?>
+  <br><br>
+  <!-- <input type="Text" name="letter_1" maxlength="1" size="1">
+  <input type="Text" name="letter_2" maxlength="1" size="1">
+  <input type="Text" name="letter_3" maxlength="1" size="1">
+  <input type="Text" name="letter_4" maxlength="1" size="1">
+  <input type="Text" name="letter_5" maxlength="1" size="1"> -->
+  <!-- <input type="Submit" value="Guess"/> -->
+  <br>
+  <?php 
+    // wordInput($css_state, $value, $word)
+    wordInput($randomWordTest);
 
+  ?>
+  </form>
 </body>
 </html>
--- wordle_functions.php
+++ wordle_functions.php
@@ -31,26 +31,125 @@ function randomWord($array_variable) {
   foreach ($array_variable[$randomWordSelect] as $singleWord) {
     yield $singleWord;
   }
-
-  
-  // return $array_variable[$randomWordSelect][0];
 }
-function guessWord($word) {
-  $guessWordArray = str_split($word);
+
+
+// function wordInput($letter_index, $css_state, $value) {
+//   echo '<input type="text" name="letter_' . $letter_index . '" maxlength="1" size="1" class="' . $css_state . '" value="' . $value .'">';
+
+// }
+
+function wordInput($css_state="standard", $value="") {
+  
+
   for ($i = 1; $i <=5; $i++) {
+    checkInput($i, $css_state="standard", $value="");
+    
+  }
+  // for ($i = 1; $i <=5; $i++) {
+  // if (isset($_POST["letter_".$i]) and !empty($_POST["letter_".$i])){
+  //   if ($word[$i-1] == $_POST["letter_".$i]) {
+  //     $css_state = "correct";
+  //     $value = $_POST["letter_".$i];
+  //   }
+  //   else {
+  //     $css_state = "wrong";
+  //     $value = $_POST["letter_".$i];
+  //     }
+  //   }
+  // }
+  echo '<input type="Submit" value="Guess"><br>';
+}
+function checkInput($word, $i, $css_state="standard", $value="") {
+  $guessWordArray = str_split($word);
+  if (!isset($_POST["letter_".$i]) and empty($_POST["letter_".$i])){
+  echo '<input type="text" name="letter_' . $i . '" maxlength="1" size="1" class="' . $css_state . '" value="' . $value .'">'; 
+  }
+  elseif (isset($_POST["letter_".$i]) and !empty($_POST["letter_".$i])){
+    if ($word[$i-1] == $_POST["letter_".$i]) {
+      $css_state = "correct";
+      $value = $_POST["letter_".$i];
+    }
+    else {
+      $css_state = "wrong";
+      $value = $_POST["letter_".$i];
+      }
+  echo '<input type="text" name="letter_' . $i . '" maxlength="1" size="1" class="' . $css_state . '" value="' . $value .'">'; 
+  }
+}
+
+  // for ($i = 1; $i <=5; $i++) {
+  //   echo '<input type="text" name="letter_' . $i . '" maxlength="1" size="1" class="' . $css_state . '" value="' . $value .'">';
+  // }
+  // if (isset($_POST["letter_1"]) and !empty($_POST["letter_1"])){
+  //   if ($word[0] == $_POST["letter_1"]) {
+  //     $css_state = "correct";
+  //     $value = $_POST["letter_1"];
+  //   }
+  //   else {
+  //     $css_state = "wrong";
+  //     $value = $_POST["letter_1"];
+  //   }
+  // }
+  // echo '<input type="Submit" value="Guess"><br>';
+
+
+
+// function wordInput($word) {
+//   $guessWordArray = str_split($word);
+//   switch (TRUE) {
+//     case (!isset($_POST["letter_1"]) and empty($_POST["letter_1"])):
+//       generateInput($i=1);
+//     case (!isset($_POST["letter_2"]) and empty($_POST["letter_2"])):
+//       generateInput($i=2);
+//     case (!isset($_POST["letter_3"]) and empty($_POST["letter_3"])):
+//       generateInput($i=3);
+//     case (!isset($_POST["letter_4"]) and empty($_POST["letter_4"])):
+//       generateInput($i=4);
+//     case (!isset($_POST["letter_5"]) and empty($_POST["letter_5"])):
+//       generateInput($i=5);
+//   }
+  
+
+//   echo '<input type="Submit" value="Guess"><br>';
+// }
+
+// function wordInput($css_state, $value, $word) {
+//   $guessWordArray = str_split($word);
+//   echo '<input type="text" name="letter_1" maxlength="1" size="1" class="' . $css_state . '" value="' . $value .'">';
+//   if (isset($_POST["letter_1"]) and !empty($_POST["letter_1"])){
+//       if ($word[0] == $_POST["letter_1"]) {
+        
+//         $css_state = "correct";
+//         $value = $_POST["letter_1"];
+//       }
+//       else {
+//         $css_state = "wrong";
+//         $value = $_POST["letter_1"];
+//         }
+    
+  
+//     echo '<input type="Submit" value="Guess"><br>';
+// }
+// }
+
+function guessWord($word) {
+  
+  $guessWordArray = str_split($word);
+  
+  for ($i = 1; $i <=5; $i++) {
+    wordInput($i, "standard", "");
     if (isset($_POST["letter_".$i]) and !empty($_POST["letter_".$i])){
       // checkWord($word);
       if ($word[$i-1] == $_POST["letter_".$i]) {
-        echo "&#10003;";
+        wordInput($i-1, "correct", $_POST["letter_".$i]);
       }
       else {
         echo "&#9587;";
       }
     }
-    else {
-      echo "<br>Try a word";
-    }
   }
+  echo '<input type="Submit" value="Guess"><br>';
 }
 
 function checkWord($word) {
