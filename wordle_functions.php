4ab4ea79012923a5d7b7ca30f1a87cd8c8c385c5
Daniel <danielpinck@gmail.com>
2024-04-11 13:17:54 +0200

    Trying git in vs code

--- wordle_functions.php
+++ wordle_functions.php
@@ -58,6 +58,7 @@ function wordInput($css_state="standard", $value="") {
   //     }
   //   }
   // }
+  // test
   echo '<input type="Submit" value="Guess"><br>';
 }
 function checkInput($word, $i, $css_state="standard", $value="") {
